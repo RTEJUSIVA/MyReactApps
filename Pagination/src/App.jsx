e635aab32d80ad0adca03a31a37fc42a89203c9a
RTEJUSIVA <santhosh.srdesigner@gmail.com>
2025-09-11 10:39:24 +0530

    Student_CRUD_operation

--- Pagination/src/App.jsx
+++ Pagination/src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Pagination from "./Components/Pagination";
+import StudentCRUD from "./Components/StudentCRUD";
 
 function App() {
   return (
     <>
-      <Pagination />
+      <StudentCRUD />
     </>
   );
 }
